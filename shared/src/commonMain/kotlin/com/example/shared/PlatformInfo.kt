b196faa76bf17216c013c86411ecaa10036171e1
Dmytro Ipatii <dmyto.ipatii@aalto.it>
2026-02-09 16:25:32 +0100

    Release version 1.0.2

--- shared/src/commonMain/kotlin/com/example/shared/PlatformInfo.kt
+++ shared/src/commonMain/kotlin/com/example/shared/PlatformInfo.kt
@@ -12,7 +12,7 @@ class PlatformInfo {
     }
 
     fun getPlatformName(): String {
-        println("Version 1.0.0")
+        println("Version 1.0.2")
         println("Platform Name: ${platform.name}")
         return platform.name
     }
